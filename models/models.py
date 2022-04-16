f365a1fc2d51384c09e66981b86bf9133d9f3b4d
Joey Giordano <joeyg@Joeys-MacBook-Air.local>
2022-04-16 13:37:00 -0400

    configuration

---
+++ models/models.py
@@ -0,0 +1,10 @@
+def sim(type):
+    """
+    Run main simulation. 
+    Args:
+        type (str): simulation run code
+    Returns: 
+        None
+    """
+
+    print(f"Running simulation: {type}.")
